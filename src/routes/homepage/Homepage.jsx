d11be4a41ee184b1e38d3fd20c7eb39f3cdeb1a4
sherard_dalaguit <sherard.dalaguit@gmail.com>
2024-08-18 16:17:46 -0700

    Add Homepage component
    Introduce a new `Homepage` component with a link to the Dashboard. This lays the groundwork for the homepage structure and navigation in the application.

---
+++ src/routes/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import './homepage.css'
+import {Link} from "react-router-dom";
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Link to="/dashboard">Dashboard</Link>
+        </div>
+    )
+}
+
+export default Homepage
