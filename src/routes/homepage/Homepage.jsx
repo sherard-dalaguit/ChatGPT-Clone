0c44b266df0c7fcc31f9c5e3bd405746ea4acb37
sherard_dalaguit <sherard.dalaguit@gmail.com>
2024-08-19 19:26:58 -0700

    Update Homepage with new layout and animated elements
    Enhanced the homepage with a new design and structure, adding an animated chat feature and improving layout responsiveness. CSS styles were updated to support these changes, including animations for visual elements and better media query handling for different screen sizes.

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,10 +1,74 @@
 import './homepage.css'
 import {Link} from "react-router-dom";
+import {TypeAnimation} from "react-type-animation";
+import {useState} from "react";
 
 const Homepage = () => {
+
+    const [typingStatus, setTypingStatus] = useState("Human1")
+
     return (
         <div className='homepage'>
-            <Link to="/dashboard">Dashboard</Link>
+            <img src="/orbital.png" alt="" className="orbital" />
+            <div className="left">
+                <h1>DALA AI</h1>
+                <h2>Supercharge your creativity and productivity</h2>
+                <h3>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Placeat sint dolorem doloribus, architecto dolor
+                </h3>
+                <Link to="/dashboard">Get Started</Link>
+            </div>
+            <div className="right">
+                <div className="imgContainer">
+                    <div className="bgContainer">
+                        <div className="bg"></div>
+                    </div>
+                    <img src="/bot.png" alt="" className="bot"/>
+                    <div className="chat">
+                        <img src={
+                            typingStatus === "Human1"
+                                ? "/human1.jpeg"
+                                : typingStatus === "Human2"
+                                    ? "/human2.jpeg"
+                                    : "/bot.png"}
+                             alt=""
+                        />
+                        <TypeAnimation
+                            sequence={[
+                                "Human: We produce food for Mice",
+                                2000, () => {
+                                    setTypingStatus("Bot")
+                                },
+                                "Bot: We produce food for Hamsters",
+                                2000, () => {
+                                    setTypingStatus("Human2")
+                                },
+                                "Human2: We produce food for Guinea Pigs",
+                                2000, () => {
+                                    setTypingStatus("Bot")
+                                },
+                                "Bot2: We produce food for Chinchillas",
+                                2000, () => {
+                                    setTypingStatus("Human1")
+                                }
+                            ]}
+                            wrapper="span"
+                            repeat={Infinity}
+                            cursor={true}
+                            omitDeletionAnimation={true}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="terms">
+                <img src="/logo.png" alt=""/>
+                <div className="links">
+                    <Link to="/">Terms of Service</Link>
+                    <span>|</span>
+                    <Link to="/">Privacy Policy</Link>
+                </div>
+            </div>
         </div>
     )
 }
